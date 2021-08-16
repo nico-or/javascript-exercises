b536b10f95c0310470b650f81119093b2521d05a
Nicolás Órdenes <nicordenes@gmail.com>
2021-08-16 19:28:29 -0400

    Add repeat function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    for (let i = 0; i < num; i++){
+        output += string;
+    }
+    return output;
 };
 
 module.exports = repeatString;
