92d5c28634bcd2362201616e4af75668e1d420c4
Nicolás Órdenes <nicordenes@gmail.com>
2021-08-18 12:40:19 -0400

    Add main function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4 != 0 ? false :
+        year % 100 != 0 ? true :
+        year % 400 != 0 ? false : true
 };
 
 module.exports = leapYears;
