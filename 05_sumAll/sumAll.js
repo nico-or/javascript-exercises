96c7bf00d57a77f12020c4cd456b2c0a2af03f44
Nicolás Órdenes <nicordenes@gmail.com>
2021-08-18 12:16:17 -0400

    Add basic funcion

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(first,last) {
+    let sum = 0;
+    for (let i = first; i <= last; i++){
+        sum += i
+    }
+    return sum
 };
 
 module.exports = sumAll;
