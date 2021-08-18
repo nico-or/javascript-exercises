7439108850479e40c02900b83089275b251fc2ab
Nicolás Órdenes <nicordenes@gmail.com>
2021-08-18 12:11:25 -0400

    Add main function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
+    // parse input
+    let array = arguments[0]
+    let toRemove = []
+    for (let i = 1; i < arguments.length; i++){
+        toRemove.push(arguments[i])
+    }
 
+    // filter values
+    let output = []
+    let current
+    for (let i = 0; i < array.length; i++){
+        current = array[i]
+        if (toRemove.includes(current) == false){
+            output.push(current)
+        }
+    }
+    return output
 };
 
 module.exports = removeFromArray;
