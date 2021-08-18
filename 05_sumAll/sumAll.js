6128d5d49f76be21d036bd755b48acb6ec866454
Nicolás Órdenes <nicordenes@gmail.com>
2021-08-18 12:22:11 -0400

    Add error handling for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,7 @@
 const sumAll = function(a,b) {
+    if (Math.min(a,b) < 0){
+        return "ERROR"
+    }
     let sum = 0
     for (let i = Math.min(a,b); i <= Math.max(a,b); i++){
         sum += i
