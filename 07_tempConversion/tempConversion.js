95ff81a98c9eac487c6ea674d4bec9fff1acf528
Nicolás Órdenes <nicordenes@gmail.com>
2021-08-18 12:55:10 -0400

    Add main function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9
 
+  /* The test suite raises and error if the function
+  returns 0.0 instead of 0.
+  This checks if the result has decimal values*/
+  return tempC % 1 == 0? tempC : tempC.toFixed(1)
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = tempC * 9 / 5 + 32
+  return tempF % 1 == 0 ? tempF : tempF.toFixed(1)
 };
 
 module.exports = {
