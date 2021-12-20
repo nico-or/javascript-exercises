9c22029d27a4576dcf5e0e276d9fcebc0c6cf17a
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-20 19:14:29 -0300

    Add string reversion function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,5 +2,13 @@ const palindromes = function () {
 
 };
 
+function reverseString(string) {
+    let reverse = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverse += string[i];
+    }
+    return reverse;
+}
+
 // Do not edit below this line
 module.exports = palindromes;
