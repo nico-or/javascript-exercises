b09107c3635e5459b24b181a0a67152a6ae256c3
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-20 19:17:26 -0300

    Add alphabetic character detection functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -10,5 +10,17 @@ function reverseString(string) {
     return reverse;
 }
 
+function isLower (char) {
+    return char >= "a" && char <= "z";
+}
+
+function isUpper (char) {
+    return char >= "A" && char <= "Z";
+}
+
+function isLetter (char) {
+    return isLower(char) || isUpper(char);
+}
+
 // Do not edit below this line
 module.exports = palindromes;
