93fc6e43faa7a0f801335c5334071ae82566db73
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-20 19:44:58 -0300

    Add main function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    for (person of people) {
+        if (getAge(person) > getAge(oldest)) {
+            oldest = person;
+        }
+    }
+    return oldest;
 };
+
+function getAge(person) {
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
